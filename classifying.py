dcc7466f35bc49d85f7c00bdf15872f2b12bb534
Minwoo Cho <chominwoo13182gmail.com>
2021-06-16 16:53:12 -0400

    added loop logic and added 'j' 'k' 'l' features

--- classifying.py
+++ classifying.py
@@ -8,8 +8,6 @@ Created on Fri Jun 11 15:42:38 2021
 import cv2 as cv
 import os
 
-path = 'images/'
-
 def image_resize(image, width = None, height = None, inter = cv.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
@@ -24,60 +22,80 @@ def image_resize(image, width = None, height = None, inter = cv.INTER_AREA):
     resized = cv.resize(image, dim, interpolation = inter)
     return resized
 
+path = 'images/'
 
-for filename in os.listdir(path):
-    flag = 1
-    if(filename.endswith(".png")):
-        img = cv.imread(path + filename)
-        copy = img.copy()
-        copy = image_resize(copy, width = 800)
-        cv.imshow('quick', copy)
-        while(flag == 1):
-            key = cv.waitKey(0)
-            if(key == ord('0')):
-                print(filename, " Saved as:  0")
-                cv.imwrite('images/0/' + filename, copy)
-                cv.imwrite('images/sorted/' + filename, img)
-                os.remove(path + filename)
-                flag = 0
-            elif(key == ord('1')):
-                print(filename, " Saved as:  1")
-                cv.imwrite('images/1/' + filename, copy)
-                cv.imwrite('images/sorted/' + filename, img)
-                os.remove(path + filename)
-                flag = 0
-            elif(key == ord('2')):
-                print(filename, " Saved as:  2")
-                cv.imwrite('images/2/' + filename, copy)
-                cv.imwrite('images/sorted/' + filename, img)
-                os.remove(path + filename)
-                flag = 0
-            elif(key == ord('3')):
-                print(filename, " Saved as:  3")
-                cv.imwrite('images/3/' + filename, copy)
-                cv.imwrite('images/sorted/' + filename, img)
-                os.remove(path + filename)
-                flag = 0
-            elif(key == ord('4')):
-                print(filename, " Saved as:  4")
-                cv.imwrite('images/4/' + filename, copy)
-                cv.imwrite('images/sorted/' + filename, img)
-                os.remove(path + filename)
-                flag = 0
-            elif(key == ord('l')):
-                print(filename, " saved for later")
-                # cv.imwrite('C:/Users/chomi/Desktop/later/' + filename, copy)
-                flag = 0
-            elif(key == ord('c')):
-                flag = 2
-            else:
-                print("Invalid key press")
-                flag = 1
-    if(flag == 2):
+print("NOTE: the window with the sample image must be in focus for this program to work.")
+print("i.e: click on the image before starting")
+print("")
+print("press '0',  '1',  '2',  '3', or '4' to classify sample.")
+print("press 'J' to come back to current sample at the end.")
+print("press 'K' check how many images are left.")
+print("press 'L' to save and quit.")
+
+cond = False
+while (len(os.listdir(path)) > 6 ):
+    imList = os.listdir(path)
+    for filename in imList:
+        flag = 1
+        if(filename.endswith(".png")):
+            img = cv.imread(path + filename)
+            copy = img.copy()
+            copy = image_resize(copy, width = 800)
+            cv.imshow('quick', copy)
+            while(flag == 1):
+                key = cv.waitKey(0)
+                if(key == ord('0')):
+                    print(filename, " Saved as:  0")
+                    cv.imwrite('images/0/' + filename, copy)
+                    cv.imwrite('images/sorted/' + filename, img)
+                    os.remove(path + filename)
+                    flag = 0
+                elif(key == ord('1')):
+                    print(filename, " Saved as:  1")
+                    cv.imwrite('images/1/' + filename, copy)
+                    cv.imwrite('images/sorted/' + filename, img)
+                    os.remove(path + filename)
+                    flag = 0
+                elif(key == ord('2')):
+                    print(filename, " Saved as:  2")
+                    cv.imwrite('images/2/' + filename, copy)
+                    cv.imwrite('images/sorted/' + filename, img)
+                    os.remove(path + filename)
+                    flag = 0
+                elif(key == ord('3')):
+                    print(filename, " Saved as:  3")
+                    cv.imwrite('images/3/' + filename, copy)
+                    cv.imwrite('images/sorted/' + filename, img)
+                    os.remove(path + filename)
+                    flag = 0
+                elif(key == ord('4')):
+                    print(filename, " Saved as:  4")
+                    cv.imwrite('images/4/' + filename, copy)
+                    cv.imwrite('images/sorted/' + filename, img)
+                    os.remove(path + filename)
+                    flag = 0
+                elif(key == ord('j')):
+                    print(filename, " saved for later")
+                    flag = 0
+                elif(key == ord('l')):
+                    print("Saving and quitting...")
+                    flag = 2
+                    cond = True
+                elif(key == ord('k')):
+                    tempList = os.listdir(path)
+                    size = len(tempList) - 6
+                    print(size, 'images left.')
+                    flag = 1
+                else:
+                    print("Invalid key press")
+                    flag = 1
+        if(flag == 2):
+            break
+    if(cond):
+        print("Saved with", len(os.listdir(path)) - 6, "images left.")
         break
-    
-print("Finished.", end=(''))
-if(flag == 2):
-    print("..User interrupt")
+if(len(os.listdir(path)) < 7):
+    print("Finished judging all samples.")
+print("closing...")
 cv.destroyAllWindows()
 
