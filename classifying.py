4a2c93a31745224e43558a79165d89d448dea38a
Minwoo Cho <chominwoo13182gmail.com>
2021-06-16 11:25:09 -0400

    ready for notebook

--- classifying.py
+++ classifying.py
@@ -8,7 +8,7 @@ Created on Fri Jun 11 15:42:38 2021
 import cv2 as cv
 import os
 
-path = 'C:/Users/chomi/Desktop/all_small/'
+path = 'images/'
 
 def image_resize(image, width = None, height = None, inter = cv.INTER_AREA):
     dim = None
@@ -36,31 +36,38 @@ for filename in os.listdir(path):
             key = cv.waitKey(0)
             if(key == ord('0')):
                 print(filename, " Saved as:  0")
-                cv.imwrite('C:/Users/chomi/Desktop/0/' + filename, copy)
+                cv.imwrite('images/0/' + filename, copy)
+                cv.imwrite('images/sorted/' + filename, img)
+                os.remove(path + filename)
                 flag = 0
             elif(key == ord('1')):
                 print(filename, " Saved as:  1")
-                cv.imwrite('C:/Users/chomi/Desktop/1/' + filename, copy)
+                cv.imwrite('images/1/' + filename, copy)
+                cv.imwrite('images/sorted/' + filename, img)
+                os.remove(path + filename)
                 flag = 0
             elif(key == ord('2')):
                 print(filename, " Saved as:  2")
-                cv.imwrite('C:/Users/chomi/Desktop/2/' + filename, copy)
+                cv.imwrite('images/2/' + filename, copy)
+                cv.imwrite('images/sorted/' + filename, img)
+                os.remove(path + filename)
                 flag = 0
             elif(key == ord('3')):
                 print(filename, " Saved as:  3")
-                cv.imwrite('C:/Users/chomi/Desktop/3/' + filename, copy)
+                cv.imwrite('images/3/' + filename, copy)
+                cv.imwrite('images/sorted/' + filename, img)
+                os.remove(path + filename)
                 flag = 0
             elif(key == ord('4')):
                 print(filename, " Saved as:  4")
-                cv.imwrite('C:/Users/chomi/Desktop/4/' + filename, copy)
-                flag = 0
-            elif(key == ord('5')):
-                print(filename, " Saved as:  5")
-                cv.imwrite('C:/Users/chomi/Desktop/5/' + filename, copy)
+                cv.imwrite('images/40/' + filename, copy)
+                cv.imwrite('images/sorted/' + filename, img)
+                os.remove(path + filename)
                 flag = 0
             elif(key == ord('l')):
                 print(filename, " saved for later")
-                cv.imwrite('C:/Users/chomi/Desktop/later/' + filename, copy)
+                # cv.imwrite('C:/Users/chomi/Desktop/later/' + filename, copy)
+                flag = 0
             elif(key == ord('p')):
                 flag = 2
             else:
